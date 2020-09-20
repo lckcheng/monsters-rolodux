4534b85f44ad303976ca0deff644617bc47c7cd5
Leo Cheng <lckcheng@gmail.com>
2020-09-20 14:35:44 +0800

    Updates

---
+++ precache-manifest.9b0f292c6f59b528a21253bab903ca24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f77ba027e51f14bb1e59e326b0aa4825",
+    "url": "/monsters-rolodux/index.html"
+  },
+  {
+    "revision": "8c2f0e8d34b78fe79ec7",
+    "url": "/monsters-rolodux/static/css/main.c3780067.chunk.css"
+  },
+  {
+    "revision": "78adac64eaa07ff35dea",
+    "url": "/monsters-rolodux/static/js/2.356d906f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodux/static/js/2.356d906f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2f0e8d34b78fe79ec7",
+    "url": "/monsters-rolodux/static/js/main.bbadd0e0.chunk.js"
+  },
+  {
+    "revision": "c0b0d209649d515ad5a8",
+    "url": "/monsters-rolodux/static/js/runtime-main.e5b9126e.js"
+  }
+]);
